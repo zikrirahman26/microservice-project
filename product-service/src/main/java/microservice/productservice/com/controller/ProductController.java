4806a87ade20207eb5d4c72dca3aba083284ad11
zikrirahman26 <zikrirahman014@gmail.com>
2025-10-06 21:41:04 +0700

    dev test

--- product-service/src/main/java/microservice/productservice/com/controller/ProductController.java
+++ product-service/src/main/java/microservice/productservice/com/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api-products")
 public class ProductController {
     //gvfgvhgasv
+    //development
 
     private final ProductService productService;
 
